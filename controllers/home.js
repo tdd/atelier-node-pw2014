a5acacb241eced9ac273a7eb804dd5105c64a2c6
Christophe Porteneuve <tdd@tddsworld.com>
2014-10-14 16:15:30 +0200

    Express : cookies de session signés

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,15 @@
 'use strict';
 
 module.exports = function homeController(app) {
+  app.use(function(req, res, next) {
+    if ('GET' === req.method) {
+      var views = req.session.views || 0;
+      req.session.views = ++views;
+    }
+    next();
+  });
+
   app.get('/', function(req, res) {
-    res.render('home', { title: 'Salut !' });
+    res.render('home', { title: 'Salut !', views: req.session.views });
   });
 };
--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 require('colors');
 
-var express = require('express');
-var http    = require('http');
-var morgan  = require('morgan');
+var cookieSession = require('cookie-session');
+var express       = require('express');
+var http          = require('http');
+var morgan        = require('morgan');
 
 var app = express();
 var server = http.createServer(app);
@@ -15,6 +16,7 @@ app.set('view engine', 'jade');
 var devMode = 'development' === app.get('env');
 
 app.use(morgan(devMode ? 'dev' : 'common'));
+app.use(cookieSession({ name: 'node-pw14:session', secret: "Node.js c’est de la balle !" }));
 
 app.locals.title = 'Node.js démystifié';
 app.locals.pretty = devMode;
