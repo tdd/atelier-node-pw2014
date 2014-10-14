16afd38b805dcb99c2e7d1a3aa03f68916130860
Christophe Porteneuve <tdd@tddsworld.com>
2014-10-14 16:38:26 +0200

    Commentaires : enregistrement de helpers, ici formatStamp

--- helpers/main.js
+++ helpers/main.js
@@ -1,8 +1,23 @@
+var moment = require('moment');
+var _ = require('underscore');
+
 module.exports = function mainHelpers(app) {
   app.use(setupHelpers);
 };
 
 function setupHelpers(req, res, next) {
   res.locals.flash = req.flash();
+
+  var lang = _.chain(req.acceptsLanguages()).invoke('split', '-').pluck(0).uniq().first().value();
+  moment.locale(lang);
+  _.extend(res.locals, HELPERS);
+
   next();
 }
+
+
+var HELPERS = {
+  formatStamp: function formatStamp(date) {
+    return moment(date).format('LLLL');
+  }
+};
