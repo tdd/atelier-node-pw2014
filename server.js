4fa0b3d30f4576e5d1e850c9e853fc11bd50528b
Christophe Porteneuve <tdd@tddsworld.com>
2014-10-14 15:56:52 +0200

    Express : logging avec morgan

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ require('colors');
 
 var express = require('express');
 var http    = require('http');
+var morgan  = require('morgan');
 
 var app = express();
 var server = http.createServer(app);
 
 app.set('port', process.env.PORT || 3000);
+
+var devMode = 'development' === app.get('env');
+
+app.use(morgan(devMode ? 'dev' : 'common'));
 
 require('./controllers/home')(app);
 
